a40640e1785b5927f383da2b3f77266869f21ed2
Jorge Luis Ordonez Ospina <joordonezo@bodegadatos.manizales.unal.edu.co>
2021-03-22 01:07:07 -0500

    Se agrega codigo para la clase control landssland alert, en base de la de control de hinundación, se agrega código para la impementación de la interface, se agregan dos clases adicionales en alertsv2 para acceder a los repositorios, se comentan rutas de api v2 para evitar ruido, pendiente corregir errores en la implementacion de la clase debido a código no asumible.

--- app/AlertSystem/ControlAlert/ControlAlertContract.php
+++ app/AlertSystem/ControlAlert/ControlAlertContract.php
@@ -2,9 +2,11 @@
 
 
 namespace App\AlertSystem\ControlAlert;
-
+use Carbon\Carbon;
 
 interface ControlAlertContract
 {
-
+    public function __construct(Carbon $dateTime);
+    public function config();
+    public function execute();
 }
--- app/AlertSystem/ControlAlert/ControlLandslideAlert.php
+++ app/AlertSystem/ControlAlert/ControlLandslideAlert.php
@@ -2,28 +2,35 @@
 
 namespace App\AlertSystem\ControlAlert;
 
-use App\AlertSystem\Alerts\AlertContract;
+use App\AlertSystem\ControlAlert\AlertContract;
+use App\AlertSystem\AlertsV2\LandslideAlert;
 use Carbon\Carbon;
 
 class ControlLandslideAlert extends ControlAlertBase implements ControlAlertContract
 {
-    /**
-     * @var AlertContract[]
-     */
-    protected $alertsSystems = [];
 
     /**
      * ControlFloodAlert constructor.
      * @param array $primaryStations
      * @param Carbon $dateTime
      */
-    public function __construct(array $primaryStations, Carbon $dateTime)
+    public function __construct( Carbon $dateTime)
     {
-        parent::__construct($primaryStations,$dateTime);
+        parent::__construct('landslide',$dateTime);
+        $this->config();
     }
 
     public function execute()
     {
+        foreach ($this->alerts as $alert){
+            $alert->execute();
+        }
+    }
 
+    public function config()
+    {
+        foreach ($this->controlAlerts as $controlAlert){
+            $this->alerts[] = new LandslideAlert($this->alertCode,$controlAlert,$this->dateTime,$this->initDateTime,$this->finalDateTime);
+        }
     }
 }
