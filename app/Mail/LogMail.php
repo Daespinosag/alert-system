ee4525d162b70e9fdb113ff08b864342e9bdac69
Jorge Luis <jorgeluisordonez1995@gmail.com>
2021-05-27 01:13:44 -0500

    create new class and template for email log

---
+++ app/Mail/LogMail.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Mail;
+
+
+class LogMail
+{
+
+}
---
+++ resources/views/emails/logEmail.blade.php
@@ -0,0 +1,7 @@
+@if(isset($data))
+<h1>{$data->type}</h1>
+<h1>{$data->status}</h1>
+<h1>{$data->priority}</h1>
+<h1>{$data->date}</h1>
+<h1>{$data->comments}</h1>
+@endif
